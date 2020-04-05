887454179019911369b7a212c61c1531121aca8e
pwildenhain <pwildenhain@gmail.com>
2020-04-04 20:28:53 -0400

    :mag: Add file browsing

--- image_viewer.py
+++ image_viewer.py
@@ -9,13 +9,35 @@ class ImagePanel(wx.Panel):
         self.image_ctrl = wx.StaticBitmap(self, bitmap=wx.Bitmap(img))
 
         browse_btn = wx.Button(self, label="Browse")
+        browse_btn.Bind(wx.EVT_BUTTON, self.on_browse)
+
+        self.photo_text = wx.TextCtrl(self, size=(200, -1))
 
         main_sizer = wx.BoxSizer(wx.VERTICAL)
+        hsizer = wx.BoxSizer(wx.HORIZONTAL)
+
+        hsizer.Add(browse_btn, 0, wx.ALL, 5)
+        hsizer.Add(self.photo_text)
+
         main_sizer.Add(self.image_ctrl, 0, wx.ALL, 5)
-        main_sizer.Add(browse_btn)
+        main_sizer.Add(hsizer, 0, wx.ALL, 5)
+
         self.SetSizer(main_sizer)
         main_sizer.Fit(parent)
         main_sizer.Layout()
+
+    def on_browse(self, event: wx.Event):
+        """Browse for an image file
+        Args:
+            event: The event object
+        """
+        wildcard = "JPEG files (*.jpg)|*.jpg"
+
+        with wx.FileDialog(
+            None, "Choose a file", wildcard=wildcard, style=wx.ID_OPEN
+        ) as dialog:
+            if dialog.ShowModal() == wx.ID_OK:
+                self.photo_text.SetValue(dialog.GetPath())
 
 
 class MainFrame(wx.Frame):
